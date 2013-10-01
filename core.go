03dcc5ced0e77c9ac3f2b49cb5ffa84817ae077d
xyplane <planar.geometry@gmail.com>
2013-09-30 21:06:54 -0400

    Add standard flags: -html, -html and -version. Implement Process() on ConfigRegistry{}.

--- core.go
+++ core.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+import "fmt"
 import "flag"
 import "sync"
 import "text/template"
@@ -98,10 +100,45 @@ func (v *ConfigValue) String() string {
 // delegates the command-line flags to Configurers.
 type ConfigRegistry struct {
 	cfgs []Configurer
-	flgs flag.FlagSet
+	flgs *flag.FlagSet
+	help bool
+	html bool
+	vrsn bool
+}
+
+// NewConfigRegistry function constructs and initializes a
+//  ConfigRegistry type, which includes the default FlagSet.
+func NewConfigRegistry() (*ConfigRegistry) {
+	c := &ConfigRegistry{ flgs:flag.NewFlagSet("groom", flag.ExitOnError) }
+	c.flgs.BoolVar(&c.help, "help", false, "Print Groom help information")
+	c.flgs.BoolVar(&c.html, "html", false, "Use HTML safe templates")
+	c.flgs.BoolVar(&c.vrsn, "version", false, "Print version information")
+	return c
 }
 
 // Register registers a mapping between a ConfigBuilder and a flag.
 func (c *ConfigRegistry) Register(cbld ConfigBuilder, name string, usage string) {
 	c.flgs.Var(&ConfigValue{ cbld:cbld, cfgs:c.cfgs }, name, usage)
+}
+
+// Process parses the command-line options and returns a slice of configurers.
+func (c *ConfigRegistry) Process() ([]Configurer, error) {
+	if !c.flgs.Parsed() {
+		c.flgs.Parse(os.Args[1:])
+	}
+	if c.help {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		c.flgs.PrintDefaults()
+		os.Exit(1)
+	}
+	if c.vrsn {
+		fmt.Fprintf(os.Stderr, "Version: %s\n", Version)
+		os.Exit(1)
+	}
+	return c.cfgs, nil
+}
+
+// IsHtml reports if the user has request HTML safe templates with the "-html" flag.
+func (c *ConfigRegistry) IsHtml() bool {
+	return c.html
 }
