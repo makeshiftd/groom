bad1aa3c8e8997529922afeedbffb77c7111da10
Dylan Maxwell <planar.geometry@gmail.com>
2013-10-06 09:15:14 -0400

    Finish 'TextTemplate' implementation of Template interface. Parse non-option arguments.

--- groom.go
+++ groom.go
@@ -3,18 +3,77 @@
 // except written in Go and using Go templates.
 package main
 
+import "io"
+import "os"
 import "fmt"
+import "bytes"
 
-var Version = "0.0.1"
+var Version = "0.1.0"
 
 var cfgReg = NewConfigRegistry()
 
 func main() {
-	cfgReg.Process();
-
-	if cfgReg.IsHtml() {
-		fmt.Println("HTML safe templates requested.")
-	} else {
-		fmt.Println("Standard text templates requested")
+	cfgs, err := cfgReg.Process();
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
 	}
+
+	var t Template
+	if cfgReg.IsHtml() {
+		fmt.Fprintln(os.Stderr, "HTML safe templates not supported (yet)")
+		os.Exit(-1)
+	} else {
+		t = NewTextTemplate()
+	}
+
+	for _, cfg := range cfgs {
+		err = cfg.Configure(t)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(-1)
+		}
+	}
+
+	var dest io.Writer = os.Stdout 
+	if cfgReg.Dest() != "-" {
+		stat, serr := os.Stat(cfgReg.Dest())
+		if serr == nil {
+			if stat.IsDir() {
+				fmt.Fprintf(os.Stderr, "destination is a directory: %s\n", cfgReg.Dest())
+				os.Exit(-1)
+			}
+			if !cfgReg.IsForce() {
+				fmt.Fprintf(os.Stderr, "destination already exists: %s\n", cfgReg.Dest())
+				os.Exit(-1)
+			}
+		}
+		file, oerr := os.Create(cfgReg.Dest())
+		if oerr != nil {
+			fmt.Fprintf(os.Stderr, "destination invalid: %s\n", cfgReg.Dest())
+			os.Exit(-1)
+		}
+		dest = file
+	}
+
+	if cfgReg.IsBuffer() {
+		buf := &bytes.Buffer{}
+		err = t.Execute(buf)
+		if err == nil {
+			_, err = buf.WriteTo(dest)
+		}
+	} else {
+		err = t.Execute(dest)
+	}
+
+	if c, ok := dest.(io.Closer); ok {
+		c.Close()
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	os.Exit(0)
 }
