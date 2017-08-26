22d24b1a9d37a58f73c2549c9ae761e0916b74a0
xyplane <planar.geometry@gmail.com>
2017-08-26 05:18:42 -0400

    Update import paths and format source code

--- groom.go
+++ groom.go
@@ -1,96 +1,94 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "regexp"
-    "strings"
-    "io/ioutil"
-    "path/filepath"
-    "./internal/template"
-    . "./internal/debug"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/makeshiftd/groom/internal/template"
+	"github.com/xyplane/debugger"
 )
 
-var debug = Debug("groom")
-
+var debug = debugger.Debug("groom")
 
 func main() {
-    os.Exit(groom(os.Args[1:]))
+	os.Exit(groom(os.Args[1:]))
 }
-
 
 func groom(args []string) int {
 
-    data, paths := parseArgs(args)
+	data, paths := parseArgs(args)
 
-    debug("Data:", data)
-    debug("Paths:", paths)
+	debug("Data:", data)
+	debug("Paths:", paths)
 
-    var err error
-    var tmpl *template.Template
+	var err error
+	var tmpl *template.Template
 
-    if len(paths) == 0 {
-        buf, rerr := ioutil.ReadAll(os.Stdin)
-        if rerr != nil {
-            fmt.Fprintln(os.Stderr, err)
-            return 1
-        }
+	if len(paths) == 0 {
+		buf, rerr := ioutil.ReadAll(os.Stdin)
+		if rerr != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 1
+		}
 
-        name := "stdin.grm"
-        path := filepath.Join(".", name)
-        tmpl, err = template.New(funcs, false).ParseText(name, path, string(buf))
-        if err != nil {
-            fmt.Fprintln(os.Stderr, err)
-            return 1
-        }
-    } else {
-        for _, path := range(paths) {
-            name := filepath.Base(path)
-            if tmpl == nil {
-                debug("New Parse File:", name, path)
-                tmpl, err = template.New(funcs, false).ParseFile(name, path)
-            } else {
-                debug("Parse File:", name, path)
-                _, err = tmpl.ParseFile(name, path)
-            }
-            if err != nil {
-                fmt.Fprintln(os.Stderr, err)
-                return 1
-            }
-        }
-    }
+		name := "stdin.grm"
+		path := filepath.Join(".", name)
+		tmpl, err = template.New(funcs, false).ParseText(name, path, string(buf))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 1
+		}
+	} else {
+		for _, path := range paths {
+			name := filepath.Base(path)
+			if tmpl == nil {
+				debug("New Parse File:", name, path)
+				tmpl, err = template.New(funcs, false).ParseFile(name, path)
+			} else {
+				debug("Parse File:", name, path)
+				_, err = tmpl.ParseFile(name, path)
+			}
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return 1
+			}
+		}
+	}
 
-    err = tmpl.Execute(os.Stdout, data)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        return 1
-    }
+	err = tmpl.Execute(os.Stdout, data)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
 
-    return 0
+	return 0
 }
-
 
 var ARG_DATA_REGEX = regexp.MustCompile("^--?((.*)\\s*=\\s*(.*?)\\s*|(.*?)\\s*)$")
 
-func parseArgs(args []string) (map[string]string, []string){
-    data := map[string]string{}
-    paths := []string{}
-    for _, arg := range args {
-        debug("Parse Arg:", arg)
-        if !strings.HasPrefix(arg, "-") {
-            paths = append(paths, arg)
-            continue
-        }
-        match := ARG_DATA_REGEX.FindStringSubmatch(arg)
-        if len(match) > 0 {
-           if match[2] != "" {
-               debug("Match Data: '%s'='%s'", match[2], match[3])
-               data[match[2]] = match[3]
-           } else if match[4] != "" {
-               debug("Match Data: '%s'=''", match[4])
-               data[match[4]] = ""
-           }
-        }
-    }
-    return data, paths
+func parseArgs(args []string) (map[string]string, []string) {
+	data := map[string]string{}
+	paths := []string{}
+	for _, arg := range args {
+		debug("Parse Arg:", arg)
+		if !strings.HasPrefix(arg, "-") {
+			paths = append(paths, arg)
+			continue
+		}
+		match := ARG_DATA_REGEX.FindStringSubmatch(arg)
+		if len(match) > 0 {
+			if match[2] != "" {
+				debug("Match Data: '%s'='%s'", match[2], match[3])
+				data[match[2]] = match[3]
+			} else if match[4] != "" {
+				debug("Match Data: '%s'=''", match[4])
+				data[match[4]] = ""
+			}
+		}
+	}
+	return data, paths
 }
--- internal/template/template.go
+++ internal/template/template.go
@@ -1,167 +1,160 @@
 package template
 
 import (
-    "os"
-    "io"
-    "regexp"
-    "errors"
-    "strings"
-    "io/ioutil"
-    "path/filepath"
-    ttemplate "text/template"
-    htemplate "html/template"
-    .  "../debug"
-)
+	"errors"
+	htemplate "html/template"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 
+	ttemplate "github.com/makeshiftd/groom/internal/template/text/template"
+	"github.com/xyplane/debugger"
+)
 
 type FuncMap map[string]interface{}
 
 type Template struct {
-    safe bool
-    tmpl interface{}
-    funcs FuncMap
+	safe  bool
+	tmpl  interface{}
+	funcs FuncMap
 }
 
-
-var debug = Debug("template")
-
+var debug = debugger.Debug("template")
 
 func New(funcs FuncMap, safe bool) *Template {
-    return &Template{ funcs:funcs, safe:safe }
+	return &Template{funcs: funcs, safe: safe}
 }
 
 func (t *Template) ParseFile(name, path string) (*Template, error) {
-    return t.parseFileWithLevel(name, path, 0)
+	return t.parseFileWithLevel(name, path, 0)
 }
 
 func (t *Template) parseFileWithLevel(name, path string, level int) (*Template, error) {
-    f, err := os.Open(path)
-    if err != nil {
-        return nil, err
-    }
-    defer f.Close()
-    buf, rerr := ioutil.ReadAll(f)
-    if rerr != nil {
-        return nil, rerr
-    }
-    return t.parseTextWithLevel(name, path, string(buf), level)
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	buf, rerr := ioutil.ReadAll(f)
+	if rerr != nil {
+		return nil, rerr
+	}
+	return t.parseTextWithLevel(name, path, string(buf), level)
 }
-
 
 func (t *Template) ParseText(name, path, text string) (tt *Template, err error) {
-    return t.parseTextWithLevel(name, path, text, 0)
+	return t.parseTextWithLevel(name, path, text, 0)
 }
-
 
 func (t *Template) parseTextWithLevel(name, path, text string, level int) (tt *Template, err error) {
-    if level == 1000 {
-        return nil, errors.New("template import level exceeded")
-    }
-    level += 1
+	if level == 1000 {
+		return nil, errors.New("template import level exceeded")
+	}
+	level += 1
 
-    imps, ierr := parseImports(text)
-    if ierr != nil {
-        return nil, ierr
-    }
+	imps, ierr := parseImports(text)
+	if ierr != nil {
+		return nil, ierr
+	}
 
-    dir := filepath.Dir(path)
-    for name, path := range(imps) {
-        if !filepath.IsAbs(path) {
-            path = filepath.Join(dir, path)
-        }
-        _, err := t.parseFileWithLevel(name, path, level)
-        if err != nil {
-            return nil, err
-        }
-    }
+	dir := filepath.Dir(path)
+	for name, path := range imps {
+		if !filepath.IsAbs(path) {
+			path = filepath.Join(dir, path)
+		}
+		_, err := t.parseFileWithLevel(name, path, level)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-    tt, err = t.parse(name, text)
-    if err != nil {
-        return nil, err
-    }
-    return tt, nil
+	tt, err = t.parse(name, text)
+	if err != nil {
+		return nil, err
+	}
+	return tt, nil
 }
-
 
 func (t *Template) Lookup(name string) *Template {
-    switch tmpl := t.tmpl.(type) {
-    case *ttemplate.Template:
-        tmpl = tmpl.Lookup(name)
-        if tmpl == nil {
-            return nil
-        }
-        return &Template{ tmpl:tmpl, safe:t.safe, funcs:t.funcs }
-    case *htemplate.Template:
-        tmpl = tmpl.Lookup(name)
-        if tmpl == nil {
-            return nil
-        }
-        return &Template{ tmpl:tmpl, safe:t.safe, funcs:t.funcs }
-    default:
-        return nil
-    }
+	switch tmpl := t.tmpl.(type) {
+	case *ttemplate.Template:
+		tmpl = tmpl.Lookup(name)
+		if tmpl == nil {
+			return nil
+		}
+		return &Template{tmpl: tmpl, safe: t.safe, funcs: t.funcs}
+	case *htemplate.Template:
+		tmpl = tmpl.Lookup(name)
+		if tmpl == nil {
+			return nil
+		}
+		return &Template{tmpl: tmpl, safe: t.safe, funcs: t.funcs}
+	default:
+		return nil
+	}
 }
 
-
 func (t *Template) Name() string {
-    switch tmpl := t.tmpl.(type) {
-    case *ttemplate.Template:
-        return tmpl.Name()
-    case *htemplate.Template:
-        return tmpl.Name()
-    default:
-        return ""
-    }
+	switch tmpl := t.tmpl.(type) {
+	case *ttemplate.Template:
+		return tmpl.Name()
+	case *htemplate.Template:
+		return tmpl.Name()
+	default:
+		return ""
+	}
 }
 
 func (t *Template) Execute(w io.Writer, data interface{}) error {
-    switch tmpl := t.tmpl.(type) {
-    case *ttemplate.Template:
-        return tmpl.Execute(w, data)
-    case *htemplate.Template:
-        return tmpl.Execute(w, data)
-    default:
-        return nil
-    }
+	switch tmpl := t.tmpl.(type) {
+	case *ttemplate.Template:
+		return tmpl.Execute(w, data)
+	case *htemplate.Template:
+		return tmpl.Execute(w, data)
+	default:
+		return nil
+	}
 }
 
-
 func (t *Template) parse(name, text string) (*Template, error) {
-    switch tmpl := t.tmpl.(type) {
-    case *ttemplate.Template:
-        debug("Parse:", name)
-        tmpl, err := tmpl.New(name).Parse(text)
-        if err != nil {
-            return nil, err
-        }
-        return &Template{ tmpl:tmpl, safe:t.safe, funcs:t.funcs }, nil
-    case *htemplate.Template:
-        debug("Parse:", name)
-        tt, err := tmpl.New(name).Parse(text)
-        if tt == nil {
-            return nil, err
-        }
-        return &Template{ tmpl:tmpl, safe:t.safe, funcs:t.funcs }, nil
-    default:
-        if t.safe {
-            debug("New:", name)
-            funcs := htemplate.FuncMap(t.funcs)
-            tmpl, err := htemplate.New(name).Funcs(funcs).Parse(text)
-            if err != nil {
-                return nil, err
-            }
-            t.tmpl = tmpl
-        } else {
-            debug("New:", name)
-            funcs := ttemplate.FuncMap(t.funcs)
-            tmpl, err := ttemplate.New(name).Funcs(funcs).Parse(text)
-            if err != nil {
-                return nil, err
-            }
-            t.tmpl = tmpl
-        }
-        return t, nil
-    }
+	switch tmpl := t.tmpl.(type) {
+	case *ttemplate.Template:
+		debug("Parse:", name)
+		tmpl, err := tmpl.New(name).Parse(text)
+		if err != nil {
+			return nil, err
+		}
+		return &Template{tmpl: tmpl, safe: t.safe, funcs: t.funcs}, nil
+	case *htemplate.Template:
+		debug("Parse:", name)
+		tt, err := tmpl.New(name).Parse(text)
+		if tt == nil {
+			return nil, err
+		}
+		return &Template{tmpl: tmpl, safe: t.safe, funcs: t.funcs}, nil
+	default:
+		if t.safe {
+			debug("New:", name)
+			funcs := htemplate.FuncMap(t.funcs)
+			tmpl, err := htemplate.New(name).Funcs(funcs).Parse(text)
+			if err != nil {
+				return nil, err
+			}
+			t.tmpl = tmpl
+		} else {
+			debug("New:", name)
+			funcs := ttemplate.FuncMap(t.funcs)
+			tmpl, err := ttemplate.New(name).Funcs(funcs).Parse(text)
+			if err != nil {
+				return nil, err
+			}
+			t.tmpl = tmpl
+		}
+		return t, nil
+	}
 }
 
 var COMMENT_REGEXP = regexp.MustCompile("^\\s*{{\\s*/\\*\\s*(.*?)\\s*\\*/}}")
@@ -169,43 +162,42 @@ var COMMENT_REGEXP = regexp.MustCompile("^\\s*{{\\s*/\\*\\s*(.*?)\\s*\\*/}}")
 var IMPORT_REGEXP = regexp.MustCompile("^\\+import\\s+(([^\\s]+)\\s+)?\"([^\"]+)\"\\s*$")
 
 func parseImports(text string) (map[string]string, error) {
-    imps := map[string]string{}
-    for {
-        comment := COMMENT_REGEXP.FindStringSubmatchIndex(text)
-        if comment == nil {
-            break;
-        }
+	imps := map[string]string{}
+	for {
+		comment := COMMENT_REGEXP.FindStringSubmatchIndex(text)
+		if comment == nil {
+			break
+		}
 
-        content := text[comment[2]:comment[3]]
-        if strings.HasPrefix(content, "+import") {
-            istmt := IMPORT_REGEXP.FindStringSubmatch(content)
-            if istmt == nil {
-                return nil, errors.New("template import error: " + content)
-            }
+		content := text[comment[2]:comment[3]]
+		if strings.HasPrefix(content, "+import") {
+			istmt := IMPORT_REGEXP.FindStringSubmatch(content)
+			if istmt == nil {
+				return nil, errors.New("template import error: " + content)
+			}
 
-            name, path := istmt[2], filepath.FromSlash(istmt[3])
-            if filepath.Ext(path) == "" {
-                path += ".grm"
-            }
-            if name == "" {
-                name = nameFromPath(path)
-            }
+			name, path := istmt[2], filepath.FromSlash(istmt[3])
+			if filepath.Ext(path) == "" {
+				path += ".grm"
+			}
+			if name == "" {
+				name = nameFromPath(path)
+			}
 
-            if p, ok := imps[name]; ok && p != path {
-                return nil, errors.New("duplicate template: " + name)
-            }
-            imps[name] = path
-        }
-        text = text[comment[1]:]
-    }
-    return imps, nil
+			if p, ok := imps[name]; ok && p != path {
+				return nil, errors.New("duplicate template: " + name)
+			}
+			imps[name] = path
+		}
+		text = text[comment[1]:]
+	}
+	return imps, nil
 }
 
-
 func nameFromPath(path string) string {
-    name := filepath.Base(path)
-    for ext := filepath.Ext(name); ext != "";  ext = filepath.Ext(name){
-        name = name[:len(name)-len(ext)]
-    }
-    return name
+	name := filepath.Base(path)
+	for ext := filepath.Ext(name); ext != ""; ext = filepath.Ext(name) {
+		name = name[:len(name)-len(ext)]
+	}
+	return name
 }
